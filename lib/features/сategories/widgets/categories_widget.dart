59dd62dbf0208e303895b928df517a62523bb9aa
2707066 <80663695+Sergey270@users.noreply.github.com>
2023-08-05 13:49:41 +0600

    add categories_widget

--- lib/features/сategories/widgets/categories_widget.dart
+++ lib/features/сategories/widgets/categories_widget.dart
@@ -13,51 +13,54 @@ class CategoriesWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Container(
-      width: 150,
-      padding: const EdgeInsets.all(10.0),
-      margin: const EdgeInsets.all(5.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        // color: Color(int.parse(product.color)),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
+    return  Stack(
 
-          GestureDetector(
-            onTap: () {
-              // Navigator.of(context)
-              //     .push(MaterialPageRoute(builder: (context) =>
-              //     ItemPage(productId: product.id)),
-              // );
-            },
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  height: 160,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      image: DecorationImage(
-                        image: NetworkImage(categories.image_url),
-                        fit: BoxFit.cover,
-                      )),
-                ),
-                Text(
-                  categories.name,
-                  style: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-
-
-        ],
-      ),
+      children:  [
+        Container(
+          height: 180,
+         // padding: const EdgeInsets.all(10.0),
+          margin: const EdgeInsets.all(14.0),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              image: DecorationImage(
+                image: NetworkImage(categories.image_url),
+                fit: BoxFit.cover,
+              )),
+        ),
+        Text(
+          categories.name,
+          style: const TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+      ],
     );
+    //   Container(
+    //   width: 150,
+    //   padding: const EdgeInsets.all(10.0),
+    //   margin: const EdgeInsets.all(5.0),
+    //   decoration: BoxDecoration(
+    //     borderRadius: BorderRadius.circular(15.0),
+    //     // color: Color(int.parse(product.color)),
+    //   ),
+    //   child: Column(
+    //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //     crossAxisAlignment: CrossAxisAlignment.stretch,
+    //     children:  [
+    //
+    //       GestureDetector(
+    //         onTap: () {
+    //           // Navigator.of(context)
+    //           //     .push(MaterialPageRoute(builder: (context) =>
+    //           //     ItemPage(productId: product.id)),
+    //           // );
+    //         },
+    //         child:
+    //       )
+    //
+    //
+    //     ],
+    //   ),
+    // );
   }
 }
  //*
