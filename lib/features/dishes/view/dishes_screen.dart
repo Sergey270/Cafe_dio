e96f8eaa2bfdbe34910f982d2a565cdcfe9d9a99
2707066 <80663695+Sergey270@users.noreply.github.com>
2023-08-19 17:32:51 +0600

    add dishes screen tegs

--- lib/features/dishes/view/dishes_screen.dart
+++ lib/features/dishes/view/dishes_screen.dart
@@ -37,98 +37,95 @@ class _DishesScreenState extends State<DishesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(categoriesName!),
+      appBar: AppBar(
+        title: Text(categoriesName!),
         elevation: 0,
       ),
-
-      body:
-
-      Column(
-        children: [
-          SizedBox(
-            height: 50,
-            // child: ListView.separated(
-            //   scrollDirection: Axis.horizontal,
-            //   padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            //   itemCount: data.tags.length,
-            //   separatorBuilder: (_, __) => const SizedBox(width: 8),
-            //   itemBuilder: (context, index) => FilterItem(
-            //     title: data.tags[index],
-            //     isActive: data.selectedTag == data.tags[index],
-            //    // onTap: () => bloc.add(ChangeDishesFilterEvent(data.tags[index])),
-            //   ),
-            // ),
-          ),
-          Expanded(
-            child: BlocBuilder<DishesBloc, DishesState>(
-              bloc: _dishesBloc,
-              builder: (context, state) {
-                if (state is DishesLoaded) {
-
-                  return Column(
-                    children: [ SizedBox(
-                      height: 50,
-                      child: ListView.separated(
-                        scrollDirection: Axis.horizontal,
-                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                        itemCount: state.dishesList.length,
-                        separatorBuilder: (_, __) => const SizedBox(width: 8),
-                        itemBuilder: (context, index) => SizedBox()
-
-
-                        //     FilterItem(
-                        //   title: data.tags[index],
-                        //   isActive: data.selectedTag == data.tags[index],
-                        //  // onTap: () => bloc.add(ChangeDishesFilterEvent(data.tags[index])),
-                        // ),
-                      ),
+      body: BlocBuilder<DishesBloc, DishesState>(
+        bloc: _dishesBloc,
+        builder: (context, state) {
+          if (state is DishesLoaded) {
+            return Column(
+              children: [
+                Expanded(
+                  //flex: 1,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: state.dishesList[5].tegs.length,
+                    itemBuilder: (context, i) {
+                       final dishes = state.dishesList[i];
+                     return Container(
+                       margin: EdgeInsets.all(6),
+                       // height: 40 ,
+                        width: 100,
+                        color: Colors.black12,
+                       child: Text(dishes.tegs[i]),
+                      );
+                    },
+                  ),
+                ),
+                Expanded(
+                   flex: 9,
+                  child: GridView.builder(
+                    padding: const EdgeInsets.all(16),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 18,
+                      crossAxisSpacing: 18,
+                      childAspectRatio: 0.74,
                     ),
-
-                      Expanded(
-                        child: GridView.builder(
-                          padding: const EdgeInsets.all(16),
-                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount (
-                           crossAxisCount: 3,
-                             mainAxisSpacing: 18,
-                            crossAxisSpacing: 18,
-                           childAspectRatio:  0.74,
-                          ),
-                          itemCount: state.dishesList.length,
-                          itemBuilder: (context, i) {
-                            final dishes = state.dishesList[i];
-                            return DishesWidget(dishes: dishes);
-                          },
-                        ),
-                      ),
-                    ],
-                  );
-                }
-                if (state is DishesLoadingFailure) {
-                  return Center(
-                    child: Column(
-                      children: [
-                        // Text(Exception as String),
-                        const Text('Нет Интернета'),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        TextButton(
-                            onPressed: () {
-                              _dishesBloc.add(LoadDishes());
-                            },
-                            child: const Text('Повторить'))
-                      ],
-                    ),
-                  );
-                }
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
-            ),
-          ),
-        ],
+                    itemCount: state.dishesList.length,
+                    itemBuilder: (context, i) {
+                      final dishes = state.dishesList[i];
+                      return DishesWidget(dishes: dishes);
+                    },
+                  ),
+                ),
+              ],
+            );
+          }
+          if (state is DishesLoadingFailure) {
+            return Center(
+              child: Column(
+                children: [
+                  // Text(Exception as String),
+                  const Text('Нет Интернета'),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        _dishesBloc.add(LoadDishes());
+                      },
+                      child: const Text('Повторить'))
+                ],
+              ),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
 }
+
+// Container(
+// child:
+// ListView.builder(
+// scrollDirection: Axis.horizontal,
+// padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+// itemCount: 4,
+// // separatorBuilder: (_, __) =>   Container(width: 8, color: Colors.amberAccent,),
+// itemBuilder: (context, i ) {
+// final dishes = state.dishesList[i];
+// Container(
+// color: Colors.blueGrey,
+// //child: Text(dishes.tegs[i]),
+// );
+// }
+//
+// ),
+// ),
